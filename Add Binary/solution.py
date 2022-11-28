ea9e769928ef24d2c815ea6baa7c0d8f01bb2c8e
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2022-11-27 19:11:28 -0500

    Created a more optimal solution to the 'Add Binary' problem.

--- Add Binary/solution.py
+++ Add Binary/solution.py
@@ -6,30 +6,35 @@ __author__      = "Giordan Andrew"
 __copyright__   = "Nov 26, 2022"
 class Solution:
     def addBinary(self, a, b):
-        int_a = binToInt(a)
-        int_b = binToInt(b)
-        sum = int_a + int_b
+        idx_1 = len(a) - 1
+        idx_2 = len(b) - 1
+        col_sum = 0
+        carry = 0
+        str_sum = ''
 
-        str_sum = str(bin(sum))
-        # print('sum: {} -> {}'.format(str_sum[2:], binToInt(str_sum[2:])))
-        return str_sum[2:]
+        while idx_1 >= 0 or idx_2 >= 0:
+            num_1 = int(a[idx_1]) if idx_1 >= 0 else 0
+            num_2 = int(b[idx_2]) if idx_2 >= 0 else 0
 
-def binToInt(bin_num):
-    int_num = 0
-    for idx, val in enumerate(bin_num):
-        # print('bin_num[{}] = {}'.format(idx, val))
-        int_num += int(val) * pow(2, len(bin_num) - (1 + idx))
+            col_sum = (num_1 + num_2 + carry) % 2
+            carry = 1 if (num_1 + num_2 + carry) > 1 else 0
+            str_sum += str(col_sum)
+            idx_1 -= 1
+            idx_2 -= 1
 
-    # print("int num = {}".format(int_num))
-    # print('\n')
-    return int_num
+        # Check if the last carry should be added to the sum
+        if carry == 1: 
+            str_sum += str(carry)
+
+        return str_sum[::-1]
+
 
 def main():
     solutionObj = Solution()
     bin_str_1 = '11'
     bin_str_2 = '1'
-    # bin_str_1 = "1010"
-    # bin_str_2 = "1011"
+    bin_str_1 = "1010"
+    bin_str_2 = "1011"
     output = solutionObj.addBinary(bin_str_1, bin_str_2)
     print("{} + {} = {}".format(bin_str_1, bin_str_2, output))
 
