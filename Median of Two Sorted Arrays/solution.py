bf8ccb96a560fa5feac095cd9543215f1f35637a
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2022-12-05 00:25:00 -0500

    Added solution to 'Median of Two Sorted Arrays' in O(log(min(x,y)) time.

--- Median of Two Sorted Arrays/solution.py
+++ Median of Two Sorted Arrays/solution.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python
 
-"""solution.py: This python file contains my solution to the 'B' problem on LeetCode."""
+"""solution.py: This python file contains my solution to the 'B' problem on LeetCode.
+   Note: A solution from Tushar Roy helped me in understanding and solving this problem in 
+         O(log(min(x,y))) time. 
+         
+   Link to Video: https://www.youtube.com/watch?v=LPFhl65R7ww&t=1480s"""
 
 __author__      = "Giordan Andrew"
-__copyright__   = "Dec 4, 2022"
+__copyright__   = "Dec 5, 2022"
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        final_list = []
-        idx_1 = 0
-        idx_2 = 0
-        # Merge sorted list
-        while idx_1 < len(nums1) or idx_2 < len(nums2):
-            try:
-                if nums1[idx_1] < nums2[idx_2]:
-                    final_list.append(nums1[idx_1])
-                    idx_1 += 1
-                else:
-                    final_list.append(nums2[idx_2])
-                    idx_2 += 1
-            except:
-                if idx_1 < len(nums1):
-                    final_list.append(nums1[idx_1])
-                    idx_1 += 1
-                else:
-                    final_list.append(nums2[idx_2])
-                    idx_2 += 1
 
-        print("The merged list is {}".format(final_list))
+        # Ensure nums1 is the smaller of the 2 arguments
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
 
-        # Odd number of elements
-        if len(final_list)%2 != 0:
-            return final_list[len(final_list)//2]
-        # Even number of elements
-        else:
-            l_mid = final_list[(len(final_list)//2) -1]
-            r_mid = final_list[(len(final_list)//2)]
-            print("{}, {}".format(l_mid, r_mid))
-            return (l_mid + r_mid)/2
+        len_x = len(nums1)
+        len_y = len(nums2)
+        low = 0
+        high = len_x 
+
+        # Use of Binary Search
+        while low <= high:
+            partition_x = (high + low) // 2
+            partition_y =  (len_x + len_y + 1)//2 - partition_x
+
+            # Determine values of 4 middle elements 
+            max_left_x = nums1[partition_x-1] if partition_x != 0 else float('-inf')
+            min_right_x = nums1[partition_x] if partition_x != len_x else float('inf')
+            max_left_y = nums2[partition_y-1] if partition_y != 0 else float('-inf')
+            min_right_y = nums2[partition_y] if partition_y != len_y else float('inf')
+
+            # Check if we have partitioned into the middle of a theoretical merged array
+            if max_left_x <= min_right_y and max_left_y <= min_right_x:
+                # Odd number of elements
+                if (len_x + len_y)%2 != 0:
+                    return max(max_left_x, max_left_y)
+                # Even number of elements
+                else:
+                    return ( max(max_left_x, max_left_y) + min(min_right_x, min_right_y) )/2
+
+            # Move our partitions more to the left
+            elif max_left_x > min_right_y:
+                high = partition_x -1
+
+            # Move our partitinon more to the right
+            else:
+                low = partition_x +1 
 def main():
     # Test Case 1
     list_1 = [1, 3]
