bb066017155edeb9f66bf39d1d93f61992fc3d5d
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2022-12-15 22:16:46 -0500

    Added solution to 'Convert Sorted Array to Binary Search Tree'.

--- Convert Sorted Array to Binary Search Tree/solution.py
+++ Convert Sorted Array to Binary Search Tree/solution.py
@@ -14,100 +14,33 @@ class TreeNode:
 
 class Solution:
     def sortedArrayToBST(self, nums):
-        
-        # Edge case of array is empty
-        if len(nums) == 0:
-            return None
+        return createBalancedTree(nums)
 
-        # Convert array into a binary tree
-        median = nums[len(nums)//2]
-        root = TreeNode(median, None, None)
+def createBalancedTree(nums):
 
-        # Create other nodes
-        for idx, val in enumerate(nums):
-            if val != root.val:
-                print("node val in loop = {}".format(val))
-                print("root val = {}".format(root.val))
-                new_node = TreeNode(val, None, None)
-                if val < root.val:
-                    print("inserting into the left subtree")
-                    insertNode(root, new_node, 'left')
-                else:
-                    print("inserting into the right subtree")
-                    insertNode(root, new_node, 'right')
-        return root
-
-def insertNode(parent_node = None, child_node = None, direction = None):
-
-    if direction == 'left':
-        # DEBUG
-        print("parent_node = {}".format(parent_node.val))
-        if parent_node.left:
-            print("left child_node = {}".format(parent_node.left.val))
-        if parent_node.right:
-            print("right child_node = {}".format(parent_node.right.val))
-        # END OF DEBUG
-
-        if parent_node and child_node.val < parent_node.val:
-            if parent_node.left == None:
-                print("inserting child_node = {} in the left node of {}\n".format(child_node.val, parent_node.val))
-                parent_node.left = child_node
-            else:
-                print("Inside recursion call for left subtree")
-                print("BEFORE - parent_node = {}".format(parent_node.val))
-                parent_node = parent_node.left
-                print("AFTER - parent_node = {}".format(parent_node.val))
-                insertNode(parent_node, child_node, 'left')
-            
-        elif parent_node and child_node.val > parent_node.val:
-            if parent_node.right == None:
-                print("inserting child_node = {} in the right node of {}\n".format(child_node.val, parent_node.val))
-                parent_node.right = child_node
-            else:
-                print("Inside recursion call for right subtree")
-                print("BEFORE - parent_node = {}".format(parent_node.val))
-                parent_node = parent_node.right
-                print("AFTER - parent_node = {}".format(parent_node.val))
-                insertNode(parent_node, child_node, 'right')
-
-
-    elif direction == 'right':
-        if parent_node and child_node.val > parent_node.val:
-            if parent_node.right == None:
-                print("inserting child_node = {} in the right node of {}\n".format(child_node.val, parent_node.val))
-                parent_node.right = child_node
-            else:
-                print("Inside recursion call for right subtree")
-                print("BEFORE - parent_node = {}".format(parent_node.val))
-                parent_node = parent_node.right
-                print("AFTER - parent_node = {}".format(parent_node.val))
-                insertNode(parent_node, child_node, 'right')
-
-        elif parent_node and child_node.val < parent_node.val:
-            if parent_node.left == None:
-                print("inserting child_node = {} in the left node of {}\n".format(child_node.val, parent_node.val))
-                parent_node.left = child_node
-            else:
-                print("Inside recursion call for left subtree")
-                print("BEFORE - parent_node = {}".format(parent_node.val))
-                parent_node = parent_node.left
-                print("AFTER - parent_node = {}".format(parent_node.val))
-                insertNode(parent_node, child_node, 'left')
-
-def inOrderTraversal(node): 
-    if node == None:
+    # Edge case of array is empty
+    if len(nums) == 0:
         return None
 
-    if node:
-        inOrderTraversal(node.left)
-        print("node val = {}".format(node.val))
-        if node.left:
-            print("child_left = {}".format(node.left.val))
-        if node.right:
-            print("child_right = {}".format(node.right.val))
-        print("\n")
-        inOrderTraversal(node.right)
+    # Convert array into a binary tree
+    median_idx = len(nums)//2
+    left_arr = nums[0:median_idx]
+    right_arr = nums[median_idx+1:len(nums)]
 
+    root = TreeNode(nums[median_idx], None, None)
+    root.left = createBalancedTree(left_arr)
+    root.right = createBalancedTree(right_arr)
+    
+    return root
+
+def inOrderTraversal(root):
+    if root == None:
+        return None
+
+    if root:
+        inOrderTraversal(root.left)
+        print(root.val)
+        inOrderTraversal(root.right)
 
 def main():
     # Binary Tree 1
@@ -119,9 +52,9 @@ def main():
 
     solutionObj = Solution()
     input = [-10, -3, 0, 5, 9]
+    input = [0,1,2,3,4,5]
     output = solutionObj.sortedArrayToBST(input)
-    # print("The output is {}".format(output))
-    print("\n\n\n\n")
+    # print("The output is {}".format(output.val))
     inOrderTraversal(output)
     
 
