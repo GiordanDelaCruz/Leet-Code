6b63546abfe34040007841a526d3c353bb6a4a0d
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2024-01-09 02:15:06 -0500

    Added attempt to solve 'Range Sum of BST'.

---
+++ Easy/Range Sum of BST/solution.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""solution.py: This python file contains my solution to the 'Range Sum of BST' problem on LeetCode."""
+
+__author__      = "Giordan Andrew"
+__copyright__   = "January 9, 2024"
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# Soltion to Leetcode Problem
+class Solution:
+        
+    def rangeSumOfBST(self, root, low, high):
+        rangeSum = 0
+
+        # Inorder traversal
+        if root:
+            # [Left Subtree]
+            self.rangeSumOfBST(root.left, low, high)
+
+            # [Root]
+            # Add value if it is within the range
+            if root.val >= low or root.val <= high:
+                rangeSum += root.val
+
+            # [Right Subtree]
+            self.rangeSumOfBST(root.right, low, high)
+
+        return rangeSum
+    
+def main():
+    # Test Data A
+    root_a = TreeNode(10, 5, 15)
+    root_a.left = TreeNode(5, 3, 7)
+    root_a.right = TreeNode(15, None, 18)
+    root_a.left.left = TreeNode(3, None, None)
+    root_a.left.right = TreeNode(7, None, None)
+    root_a.right.right = TreeNode(18, None, None)
+    low_a = 7
+    high_a = 15
+
+    solutionObj = Solution()
+    output_a = solutionObj.rangeSumOfBST(root_a, low_a, high_a)
+
+    print("[Test Data A]: The range sum is {}".format(output_a))
+
+main()
