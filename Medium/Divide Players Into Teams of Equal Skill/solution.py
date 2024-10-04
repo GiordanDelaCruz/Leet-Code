6f768d2f8da8a25a9de77844c4ac550b49ca31ba
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2024-10-04 17:52:13 -0400

    Added solution to 'Divide Players Into Teams of Equal Skill'.

---
+++ Medium/Divide Players Into Teams of Equal Skill/solution.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""solution.py: This python file contains my solution to the 'Divide Players Into Teams of Equal Skill' problem on LeetCode.
+                
+                Note: A solution from with aarzoo on leetcode and a solution from Code Thoughts on 
+                      YouTube helped me understand and solve the problem.   
+
+                Link to video: https://www.youtube.com/watch?v=1hjPA6YbulE    
+"""
+__author__      = "Giordan Andrew"
+__copyright__   = "October 4, 2024"
+
+class Solution:
+    def dividePlayers(self, skill):
+        # Sort the skill of players
+        players = sorted(skill)
+        print("\n[players] = {}".format(players))
+
+        # Pair up the teams
+        left = len(players) - 1
+        right = 0
+        teams = {}
+        while right < left:
+            teams[right] = [ players[right], players[left] ]
+            right += 1
+            left -= 1
+        print("[teams] = {}".format(teams))
+
+        # Check if all teams are of equal skill
+        total_skill = teams[0][0] + teams[0][1]
+        chemistry_arr = []
+        print("[total_skill] = {}".format(total_skill))
+        for idx, team in enumerate(teams):
+            chemistry_arr.append(teams[idx][0] * teams[idx][1])
+            current_team_skill = teams[idx][0] + teams[idx][1]
+            if current_team_skill != total_skill:
+                return -1
+        
+        # Calcualte total chemistry
+        total_chemistry = sum(chemistry_arr)
+        return total_chemistry
+
+def main():
+    solutionObj = Solution()
+
+    # Test Data
+    arr1 = [3,2,5,1,3,4]
+    arr2 = [3,4]
+    arr3 = [1,1,2,3]
+
+    # Outputs
+    output1 = solutionObj.dividePlayers(arr1)
+    output2 = solutionObj.dividePlayers(arr2)
+    output3 = solutionObj.dividePlayers(arr3)
+
+    # Display Results
+    print("\n[Output1]: For array = {} is {}".format(arr1, output1))
+    print("[Output2]: For array = {} is {}".format(arr2, output2))
+    print("[Output3]: For array = {} is {}".format(arr3, output3))
+
+main()
---
+++ template_solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""solution.py: This python file contains my solution to the '[PROBLEM]' problem on LeetCode.
+                
+                Note: A solution from with [USER] on [PLATFORM] helped me understand and solve the problem.   
+
+                Link to video: [URL_LINK]
+"""
+__author__      = "Giordan Andrew"
+__copyright__   = "October 4, 2024"
+
+class Solution:
+    def functionName():
+        pass
+
+def main():
+    solutionObj = Solution()
+
+    # Test Data
+    arr1 = []
+
+    # Outputs
+    output1 = solutionObj.functionName(arr1)
+
+    # Display Results
+    print("\n[Output1]: For array = {} with p = {} is {}".format(arr1, output1))
+
+main()
